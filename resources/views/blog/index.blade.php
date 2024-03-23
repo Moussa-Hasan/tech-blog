abe73bd44ac4c460b56d325c8b321a1b0ae4f34b
Moussa <moussa.hasan.99@gmail.com>
2024-03-23 15:08:51 +0200

    Install daisyUI

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -7,11 +7,10 @@
 
     @if (Auth::check())
         <div class="flex justify-center">
-            <a class="text-white bg-green-500 py-2 px-3 mt-2 rounded-lg hover:bg-green-400 font-bold uppercase duration-300"
+            <a class="btn text-white bg-green-500 py-2 px-3 mt-2 rounded-lg hover:bg-green-400 font-bold uppercase duration-300"
                 href="/blog/create">Add new post</a>
         </div>
     @endif
-
 
     @if (session()->has('message'))
         <div id="alert" class="flex justify-center mt-2">
@@ -38,7 +37,7 @@
                     <div class="prose prose-slate prose-sm text-slate-600 dark:prose-dark">
                         <p>{!! substr($post->description, 0, strrpos(substr($post->description, 0, 120), ' ')) !!}...</p>
                     </div>
-                    <a class="group inline-flex items-center h-9 rounded-full text-sm font-semibold whitespace-nowrap px-3 focus:outline-none focus:ring-2 bg-slate-100 text-slate-700 hover:bg-slate-200 hover:text-slate-900 focus:ring-slate-500 dark:bg-slate-700 dark:text-slate-100 dark:hover:bg-slate-600 dark:hover:text-white dark:focus:ring-slate-500 mt-6"
+                    <a class="btn group inline-flex items-center h-9 rounded-full text-sm font-semibold whitespace-nowrap px-3 focus:outline-none focus:ring-2 bg-slate-100 text-slate-700 hover:bg-slate-200 hover:text-slate-900 focus:ring-slate-500 dark:bg-slate-700 dark:text-slate-100 dark:hover:bg-slate-600 dark:hover:text-white dark:focus:ring-slate-500 mt-6"
                         href="/blog/{{ $post->slug }}">
                         Learn more
                         <span class="sr-only">
